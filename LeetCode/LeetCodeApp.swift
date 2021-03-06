430baaf2127df6bf8f02066fd40e5d4de82617e1
dos Santos, Diego <diego.dos.santos@sap.com>
2021-03-06 17:44:36 -0300

    Initial Commit

---
+++ LeetCode/LeetCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeApp.swift
+//  LeetCode
+//
+//  Created by dos Santos, Diego on 06/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
